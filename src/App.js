a59b8a97ab32f92f0c9f644a22d48da8a3552dc4
dev_johsire <josiah5115@gmail.com>
2019-05-22 00:35:34 -0600

    Map Character Array

--- src/App.js
+++ src/App.js
@@ -2,10 +2,33 @@ import React, { Component } from "react";
 import Table from "./Table";
 
 class App extends Component {
+  state = {
+    characters: [],
+  }
+
   render() {
+    const characters = [
+      {
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        name: "Dee",
+        job: "Actress"
+      },
+      {
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ];
+
     return (
       <div className="container">
-        <Table />
+        <Table characterData={characters} />
       </div>
     );
   }
--- src/Table.js
+++ src/Table.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
-import TableHeader from "./TableHeader";
-import TableBody from "./TableBody";
 
 class Table extends Component {
   render() {
+    const { characterData } = this.props;
+
     return (
       <table>
         <TableHeader />
-        <TableBody />
+        <TableBody characterData={characterData} />
       </table>
     );
   }
+}
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    );
+}
+
+const TableBody = props => {
+  const rows = props.characterData.map((row, index) => {
+    return (
+      <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+      </tr>
+    )
+})
+
+return <tbody>{rows}</tbody>
 }
 
 export default Table;
