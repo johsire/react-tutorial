5e7dea7785b95b5699dfb7d6836a6a984c08ad46
dev_johsire <josiah5115@gmail.com>
2019-05-22 19:50:02 -0600

    Updates

---
+++ precache-manifest.bb91100fa1c37f3aa75b58efd56ce0f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20afc39d233a4aade86d4e10ff7f556",
+    "url": "/basic-react-tutorial/index.html"
+  },
+  {
+    "revision": "c8f29c0dcb96ca5fe8c3",
+    "url": "/basic-react-tutorial/static/css/main.40540556.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/basic-react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "c8f29c0dcb96ca5fe8c3",
+    "url": "/basic-react-tutorial/static/js/main.6f0faa55.chunk.js"
+  },
+  {
+    "revision": "1e0489175f224052883c",
+    "url": "/basic-react-tutorial/static/js/runtime~main.1cd52f50.js"
+  }
+]);
